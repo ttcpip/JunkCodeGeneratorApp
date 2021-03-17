7073ef582ee6aaa50495d1b1a6d46d17645bd7c9
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-17 19:24:33 +0600

    Added random integers and string generator

---
+++ src/RandomGenarator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkCodeGeneratorApp.src
+{
+    public class RandomGenarator
+    {
+        // Instantiate random number generator.  
+        // It is better to keep a single Random instance 
+        // and keep using Next on the same instance.  
+        private readonly Random random = new Random();
+
+        // Generates a random number within a range.      
+        public int Int(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        // Generates a random string with a given size.    
+        public string String(int size, bool lowerCase = false)
+        {
+            var builder = new StringBuilder(size);
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):   
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length = 26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+    }
+}
