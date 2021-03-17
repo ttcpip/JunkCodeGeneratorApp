2ecc4d137666fdbb4579d7dc864c71415583e8d9
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-17 19:01:56 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using JunkCodeGeneratorApp.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkCodeGeneratorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new CodeGenerator();
+            
+        }
+    }
+}
