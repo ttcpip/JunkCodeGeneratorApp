b05ebfff91709da16cf7a95edea46240b2d354d6
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-18 21:17:30 +0600

    RandomGenarator.String method now have configurable chars string

--- src/RandomGenarator.cs
+++ src/RandomGenarator.cs
@@ -8,38 +8,20 @@ namespace JunkCodeGeneratorApp.src
 {
     public class RandomGenarator
     {
-        // Instantiate random number generator.  
-        // It is better to keep a single Random instance 
-        // and keep using Next on the same instance.  
         private readonly Random random = new Random();
 
-        // Generates a random number within a range.      
         public int Int(int min, int max)
         {
             return random.Next(min, max);
         }
 
-        // Generates a random string with a given size.    
-        public string String(int size, bool lowerCase = false)
+        public string String(int length, string chars = @"abcdefghijklmnopqstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
         {
-            var builder = new StringBuilder(size);
+            string k = new string(
+                Enumerable.Range(0, length).Select((_) => chars[Int(0, chars.Length)]).ToArray()
+            );
 
-            // Unicode/ASCII Letters are divided into two blocks
-            // (Letters 65–90 / 97–122):   
-            // The first group containing the uppercase letters and
-            // the second group containing the lowercase.  
-
-            // char is a single Unicode character  
-            char offset = lowerCase ? 'a' : 'A';
-            const int lettersOffset = 26; // A...Z or a..z: length = 26  
-
-            for (var i = 0; i < size; i++)
-            {
-                var @char = (char)random.Next(offset, offset + lettersOffset);
-                builder.Append(@char);
-            }
-
-            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+            return k;
         }
     }
 }
