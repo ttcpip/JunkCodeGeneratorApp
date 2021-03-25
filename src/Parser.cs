a54791aeb2be23140972828a4b145b5655b2be4d
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-25 18:24:34 +0600

    Placeholders class is now outside of Parser.cs

--- src/Parser.cs
+++ src/Parser.cs
@@ -2,30 +2,6 @@
 
 namespace JunkCodeGeneratorApp.src
 {
-    static public class Placeholders
-    {
-        public const string RANDOM_STRING_VALUE = "RANDOM_STRING_VALUE";
-        public const string RANDOM_INT_VALUE = "RANDOM_INT_VALUE";
-        public const string RANDOM_BYTE_VALUE = "RANDOM_BYTE_VALUE";
-        public const string RANDOM_DECIMAL_VALUE = "RANDOM_DECIMAL_VALUE";
-        public const string RANDOM_STRING_ARRAY_VALUE = "RANDOM_STRING_ARRAY_VALUE";
-        public const string RANDOM_INT_ARRAY_VALUE = "RANDOM_INT_ARRAY_VALUE";
-        public const string RANDOM_BYTE_ARRAY_VALUE = "RANDOM_BYTE_ARRAY_VALUE";
-        public const string RANDOM_DECIMAL_ARRAY_VALUE = "RANDOM_DECIMAL_ARRAY_VALUE";
-
-        public static string[] PlaceholdersArray = new string[]
-        {
-            RANDOM_STRING_VALUE,
-            RANDOM_INT_VALUE,
-            RANDOM_BYTE_VALUE,
-            RANDOM_DECIMAL_VALUE,
-            RANDOM_STRING_ARRAY_VALUE,
-            RANDOM_INT_ARRAY_VALUE,
-            RANDOM_BYTE_ARRAY_VALUE,
-            RANDOM_DECIMAL_ARRAY_VALUE,
-        };
-    }
-
     public class Parser
     {
         private const string STRING_BY_PLACEHOLDER_NOT_FOUND_RETURNING_STRING = "/* string not found */";
---
+++ src/Placeholders.cs
@@ -0,0 +1,26 @@
+﻿namespace JunkCodeGeneratorApp.src
+{ 
+    static public class Placeholders
+    {
+        public const string RANDOM_STRING_VALUE = "RANDOM_STRING_VALUE";
+        public const string RANDOM_INT_VALUE = "RANDOM_INT_VALUE";
+        public const string RANDOM_BYTE_VALUE = "RANDOM_BYTE_VALUE";
+        public const string RANDOM_DECIMAL_VALUE = "RANDOM_DECIMAL_VALUE";
+        public const string RANDOM_STRING_ARRAY_VALUE = "RANDOM_STRING_ARRAY_VALUE";
+        public const string RANDOM_INT_ARRAY_VALUE = "RANDOM_INT_ARRAY_VALUE";
+        public const string RANDOM_BYTE_ARRAY_VALUE = "RANDOM_BYTE_ARRAY_VALUE";
+        public const string RANDOM_DECIMAL_ARRAY_VALUE = "RANDOM_DECIMAL_ARRAY_VALUE";
+
+        public static string[] PlaceholdersArray = new string[]
+        {
+            RANDOM_STRING_VALUE,
+            RANDOM_INT_VALUE,
+            RANDOM_BYTE_VALUE,
+            RANDOM_DECIMAL_VALUE,
+            RANDOM_STRING_ARRAY_VALUE,
+            RANDOM_INT_ARRAY_VALUE,
+            RANDOM_BYTE_ARRAY_VALUE,
+            RANDOM_DECIMAL_ARRAY_VALUE,
+        };
+    }
+}
